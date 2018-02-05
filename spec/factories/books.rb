05919abb02ee756c74c01d05003aab64de868f26
Dmitriy Bavykin <1996@3g.ua>
2018-02-05 17:16:00 +0200

    refactoring factories

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description { FFaker::Book.description }
     price { rand(1.1...99.9).to_f.truncate(2) }
     quantity { rand(1..100) }
-    category_id { Category.find(rand(1..Category.count)).id }
-    author_id { Author.find(rand(1..Author.count)).id }
+    category_id { Category.find(rand(Category.first.id..Category.last.id)).id }
+    author_id { Author.find(rand(Author.first.id..Author.last.id)).id }
   end
 end
--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :rating do
     review { FFaker::Tweet.body }
     rate { rand(1..10) }
-    book_id { Book.find(rand(1..Book.count)).id }
-    customer_id { Customer.find(rand(1..Customer.count)).id }
+    book_id { Book.find(rand(Book.first.id..Book.last.id)).id }
+    customer_id { Customer.find(rand(Customer.first.id..Customer.last.id)).id }
   end
 end
