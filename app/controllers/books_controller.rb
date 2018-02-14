bf4bac08e0c7ff96fecf297ee16bc51f8bc5554b
Dmitriy Bavykin <1996@3g.ua>
2018-02-14 16:06:47 +0200

    some views changed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
-    render template: 'books/show'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def qunatity_book_in_category(id)
     Book.where(category_id: id).length
   end
+
+  def book_main_image(book)
+    book.images.any? ? book.images.first.path : 'default'
+  end
 end
