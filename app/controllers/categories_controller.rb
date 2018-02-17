7185a31bb3b532009c44529282cde6f8253b7d81
Dmitriy Bavykin <1996@3g.ua>
2018-02-17 15:09:50 +0200

    filters implemented

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
   def index
-    @books = Book.page(params[:page])
+    @filter = params[:filter] || :newest
+    @books = Book.public_send(@filter).page(params[:page])
   end
 
   def show
+    @filter = params[:filter] || :newest
     @category = Category.find(params[:id])
-    @books = Book.where(category_id: @category).page(params[:page])
+    @books = Book.where(category_id: @category).public_send(@filter).page(params[:page])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,22 @@
 class Book < ApplicationRecord
+  FILTERS = {
+    newest: 'Newest first',
+    popular: 'Popular first',
+    low_to_high: 'Price: Low to high',
+    high_to_low: 'Price: High to low',
+    by_name: 'Title: A - Z',
+    by_name_desc: 'Title: Z - A'
+  }.freeze
   has_many :ratings
   has_many :images
   belongs_to :category
   belongs_to :author
   validates :title, :price, :quantity, presence: true
-  default_scope { order('created_at') }
   scope :for_slider, -> { last(3) }
   scope :best_sellers, -> { last(4) }
+  scope :newest, -> { order('created_at DESC') }
+  scope :low_to_high, -> { order('price') }
+  scope :high_to_low, -> { order('price DESC') }
+  scope :by_name, -> { order('title') }
+  scope :by_name_desc, -> { order('title DESC') }
 end
