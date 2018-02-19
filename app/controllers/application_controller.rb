a750027244be44e101cd01d3df4d11fd94e27f93
Dmitriy Bavykin <1996@3g.ua>
2018-02-19 16:02:49 +0200

    making a guest user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  [CanCan::AccessDenied, ActiveRecord::RecordNotFound, ActionController::RoutingError].each do |error|
+    rescue_from error do |exception|
+      redirect_to main_app.root_path, alert: exception.message
+    end
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user_or_guest)
+  end
+
+  def current_user_or_guest
+    current_user ? current_user : guest_user
+  end
+
+  def guest_user
+    return User.find(session[:guest_user_id]) if session[:guest_user_id]
+    user_guest = User.new(:email => "guest_#{Time.now.to_i}#{rand(100)}@guest.com")
+    user_guest.save!(:validate => false)
+    session[:guest_user_id] = user_guest.id
+    user_guest
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+  root 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :categories, only: %i[index show]
   resources :books
