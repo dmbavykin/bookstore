944d725db1b85bbaa2b68c2047034e4d8a0eaa3b
Dmitriy Bavykin <1996@3g.ua>
2018-02-19 12:11:44 +0200

    ref

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description { FFaker::Book.description }
     price { rand(1.1...99.9).to_f.truncate(2) }
     quantity { rand(1..100) }
-    category_id { Category.find(rand(Category.first.id..Category.last.id)).id }
-    author_id { Author.find(rand(Author.first.id..Author.last.id)).id }
+    author
+    category
   end
 end
--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :image do
     path { 'image/upload/v1518298646/default.jpg' }
-    book_id { Book.find(rand(Book.first.id..Book.last.id)).id }
+    book
   end
 end
